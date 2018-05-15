0d3d9fb6df73b8c0e5efeeac283e956fcf5cdb32
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2018-05-15 13:43:27 +0800

    update: ui effect done.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,62 +4,74 @@ import _ from "lodash";
 
 const navData = [
   {
+    key: _.uniqueId(),
     title: "警情管理",
-    items: [{ label: "当前警情", uri: "" }, { label: "历史警情", uri: "" }],
-    active: true
+    items: [
+      { key: _.uniqueId(), label: "当前警情", uri: "" },
+      { key: _.uniqueId(), label: "历史警情", uri: "" }
+    ]
   },
   {
+    key: _.uniqueId(),
     title: "资源管理",
     items: [
-      { label: "资源人", uri: "" },
-      { label: "资源车", uri: "" },
-      { label: "资源物资", uri: "" }
-    ],
-    active: false
+      { key: _.uniqueId(), label: "资源人", uri: "" },
+      { key: _.uniqueId(), label: "资源车", uri: "" },
+      { key: _.uniqueId(), label: "资源物资", uri: "" }
+    ]
   }
 ];
 
 function NavPage(props) {
-  const page = props.page;
+  function togglePage(e) {
+    e.preventDefault();
+
+    let to = !props.active;
+
+    if (_.isFunction(props.onPageActive))
+      props.onPageActive(to ? props.page.key : "");
+  }
+
   return (
     <div
-      className={["nav-page", page.active ? "nav-page-active" : ""].join(" ")}
+      className={["nav-page", props.active ? "nav-page-active" : ""].join(" ")}
+      onClick={togglePage}
     >
       <div className="nav-page-title sidebar-trans">
-        <span>{page.title}</span>
+        <span>{props.page.title}</span>
       </div>
-      <NavItemList items={page.items} active={page.active} />
+      <NavItemList items={props.page.items} active={props.active} />
     </div>
   );
 }
 
 function NavItemList(props) {
-  const items = props.items;
-  let listStyle = { maxHeight: props.active ? _.size(items) * 40 : 0 };
+  let items = props.items;
+  let listStyle = { maxHeight: (props.active ? items.length * 40 : 0) + "px" };
   return (
     <ul className="sidebar-trans" style={listStyle}>
-      {_.map(items, item => <NavItem item={item} />)}
+      {items.map(item => (
+        <li key={item.key}>
+          <a href={item.uri}>{item.label}</a>
+        </li>
+      ))}
     </ul>
-  );
-}
-
-function NavItem(props) {
-  const item = props.item;
-  return (
-    <li>
-      <a href={item.uri}>{item.label}</a>
-    </li>
   );
 }
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
+    this.data = navData;
     this.state = {
-      pages: navData,
-      activePage: "",
-      activeMaxHeight: 80
+      activePageId: this.data[0].key
     };
+
+    this.handlePageActive = this.handlePageActive.bind(this);
+  }
+
+  handlePageActive(pageId) {
+    this.setState(prevState => ({ activePageId: pageId }));
   }
 
   render() {
@@ -68,7 +80,14 @@ class Sidebar extends Component {
         <div className="toggle">
           <a href="">| | |</a>
         </div>
-        {_.map(this.state.pages, page => <NavPage page={page} />)}
+        {this.data.map(page => (
+          <NavPage
+            key={page.key}
+            page={page}
+            active={page.key === this.state.activePageId}
+            onPageActive={this.handlePageActive}
+          />
+        ))}
       </div>
     );
   }
